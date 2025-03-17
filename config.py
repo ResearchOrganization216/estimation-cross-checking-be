9d4b95fdd52d360d4d7191c09084d3ad9c571bed
it21257568 <it21257568@my.sliit.lk>
2025-03-17 15:01:04 +0530

    Modifies postgres-deployment.yml file

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
      # For local development, provide a key file; in production, you can set this to an empty string or None.
     #KEY_PATH = "C:\\Users\\Sithija\\Documents\\keyfiles\\proven-answer-451215-k1-190fdb8da7df.json"
     KEY_PATH = ""  # Set to an empty string in production
-    PROJECT_ID = "proven-answer-451215-k1"
+    PROJECT_ID = "innoainsure-project"
     SECRET_KEY = 'your-very-secret-key'
