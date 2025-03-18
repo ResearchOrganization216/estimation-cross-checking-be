fd4b8ac72b56d4fb07600e7e72780e1f45e0725a
it21257568 <it21257568@my.sliit.lk>
2025-03-19 00:03:45 +0530

    Modifies estimation-routes

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@34.142.175.163:5432/InnoAInsure'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@34.126.101.125:5432/InnoAInsure'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BUCKET_NAME = "innoa-bucket"
     KEY_PATH = ""  # Set to an empty string in production
